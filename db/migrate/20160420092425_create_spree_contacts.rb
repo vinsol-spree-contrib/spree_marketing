4ceff675a527530a29c70b1f5c09dd523752e1f4
VINAY MITTAL <vinay@vinsol.com>
2016-04-20 16:42:18 +0530

    optimize indexes in migrations

--- db/migrate/20160420092425_create_spree_contacts.rb
+++ db/migrate/20160420092425_create_spree_contacts.rb
@@ -3,9 +3,9 @@ class CreateSpreeContacts < ActiveRecord::Migration
     create_table :spree_contacts do |t|
       t.string :mailchimp_id
       t.string :uid, null: false
-      t.string :email, null: false
-      t.boolean :active, default: true, index: true, null: false
-      t.index [:email, :active]
+      t.string :email, null: false, index: true
+      t.boolean :active, default: true, null: false
+      t.index [:active, :email]
     end
   end
 end
--- db/migrate/20160420093515_create_spree_lists.rb
+++ db/migrate/20160420093515_create_spree_lists.rb
@@ -2,9 +2,9 @@ class CreateSpreeLists < ActiveRecord::Migration
   def change
     create_table :spree_lists do |t|
       t.string :uid, null: false
-      t.string :name
-      t.boolean :active, default: true, index: true, null: false
-      t.index [:name, :active]
+      t.string :name, index: true
+      t.boolean :active, default: true, null: false
+      t.index [:active, :name]
     end
   end
 end
