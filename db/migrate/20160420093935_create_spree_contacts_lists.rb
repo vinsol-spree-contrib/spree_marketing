2462dc56afffaefa4389a0b314395a0ee818ad59
VINAY MITTAL <vinay@vinsol.com>
2016-04-20 16:37:26 +0530

    change indexing in join table

--- db/migrate/20160420093935_create_spree_contacts_lists.rb
+++ db/migrate/20160420093935_create_spree_contacts_lists.rb
@@ -2,7 +2,7 @@ class CreateSpreeContactsLists < ActiveRecord::Migration
   def change
     create_table :spree_contacts_lists do |t|
       t.references :contact, index: true
-      t.references :list, index: true
+      t.references :list
       t.index [:list_id, :contact_id]
       t.boolean :active, default: true, index: true, null: false
     end
