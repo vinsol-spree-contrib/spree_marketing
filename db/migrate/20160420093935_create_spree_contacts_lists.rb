7e57ffa4f64fbc1f0907efb23a2b5641daa470fc
VINAY MITTAL <vinay@vinsol.com>
2016-04-20 16:33:25 +0530

    fix error in migration

--- db/migrate/20160420093935_create_spree_contacts_lists.rb
+++ db/migrate/20160420093935_create_spree_contacts_lists.rb
@@ -1,10 +1,10 @@
 class CreateSpreeContactsLists < ActiveRecord::Migration
   def change
     create_table :spree_contacts_lists do |t|
-      t.references :contacts, index: true
-      t.references :lists, index: true
+      t.references :contact, index: true
+      t.references :list, index: true
       t.index [:list_id, :contact_id]
-      t.boolean :active, default: true, index: true
+      t.boolean :active, default: true, index: true, null: false
     end
   end
 end
