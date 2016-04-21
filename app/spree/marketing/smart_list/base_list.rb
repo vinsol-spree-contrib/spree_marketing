452ccf9655cf7571feaa59122ad86fb81965d8ae
VINAY MITTAL <vinay@vinsol.com>
2016-04-21 19:19:37 +0530

    change method names according to discussion

--- app/spree/marketing/smart_list/base_list.rb
+++ app/spree/marketing/smart_list/base_list.rb
@@ -16,25 +16,25 @@ module Spree
           @time_frame = time_frame || TIME_FRAME
         end
 
-        def fetch_list_records
-          # @list_uid ? MailChimpApi::Call @list_uid || []
-        end
+        # def fetch_list_records
+        #   # @list_uid ? MailChimpApi::Call @list_uid || []
+        # end
 
-        def contacts
-          emails - fetch_list_records
-        end
+        # def new_emails
+        #   emails - fetch_list_records
+        # end
 
         def emails
           Spree.user_class.all.pluck(:email)
         end
 
-        def process
-          # Spree::List.process_new_list contacts, @list_uid
-        end
+        # def process
+        #   # Spree::List.process_new_list contacts, @list_uid
+        # end
 
-        def self.process
-          # new.process
-        end
+        # def self.process
+        #   # new.process
+        # end
 
         def computed_time
           Time.current - time_frame
