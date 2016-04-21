afa4c07b54d739bca5e470efa7b3b271bad7f071
VINAY MITTAL <vinay@vinsol.com>
2016-04-21 19:19:37 +0530

    change method names according to discussion

--- app/spree/marketing/smart_list/abandoned_cart_list.rb
+++ app/spree/marketing/smart_list/abandoned_cart_list.rb
@@ -3,14 +3,16 @@ module Spree
     module SmartList
       class AbandonedCartList < BaseList
         def emails
-          # There is a case where guest user has an inomplete order and we
+          # There is a case where guest user has an incomplete order and we
           # might have his email if he has processed address state successfully
           # right now we are leaving that case.
-          Spree::Order.incomplete
-                      .where("spree_orders.updated_at >= :time_frame", time_frame: computed_time)
-                      .where.not(user_id: nil)
-                      .uniq
-                      .pluck(:email)
+          user_ids = Spree::Order.incomplete
+                                 .where("spree_orders.updated_at >= :time_frame", time_frame: computed_time)
+                                 .where.not(user_id: nil)
+                                 .uniq
+                                 .pluck(:user_id)
+
+          Spree::User.where(id: user_ids).pluck(:email)
         end
       end
     end
