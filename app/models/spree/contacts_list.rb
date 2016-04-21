08942e808a72ee1820a40bc2fba206e50303b06b
VINAY MITTAL <vinay@vinsol.com>
2016-04-21 12:10:57 +0530

    split models section wise

--- app/models/spree/contacts_list.rb
+++ app/models/spree/contacts_list.rb
@@ -1,6 +1,7 @@
 module Spree
   class ContactsList < ActiveRecord::Base
 
+    # Validations
     belongs_to :contact, class_name: "Spree::Contact"
     belongs_to :list, class_name: "Spree::List"
 
