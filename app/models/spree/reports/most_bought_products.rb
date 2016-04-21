86226437d1c2174efc9caf95fed757ca3f78204f
VINAY MITTAL <vinay@vinsol.com>
2016-04-21 11:27:03 +0530

    change count values wherever required

--- app/models/spree/reports/most_bought_products.rb
+++ app/models/spree/reports/most_bought_products.rb
@@ -1,17 +1,17 @@
 module Spree
   module Reports
     class MostBoughtProducts < Base
-      def initialize count = 10
-        @count = count
-      end
-
       def query
         Spree::InventoryUnit.includes(variant: :product)
                             .group("spree_variants.id")
                             .order("COUNT(spree_inventory_units.id) DESC")
                             .references(:variant)
-                            .limit(@count)
+                            .limit(count)
                             .map { |inventory_unit| inventory_unit.variant.product }
+      end
+
+      def count
+        10
       end
     end
   end
--- app/models/spree/reports/most_searched_keywords.rb
+++ app/models/spree/reports/most_searched_keywords.rb
@@ -1,15 +1,15 @@
 module Spree
   module Reports
     class MostSearchedKeywords < Base
-      def intitialize count = 10
-        @count = count
-      end
-
       def query
         Spree::PageEvent.where(action: "search")
                         .group(:search_keywords)
                         .order("COUNT(spree_page_events.id) DESC")
-                        .limit(@count)
+                        .limit(count)
+      end
+
+      def count
+        10
       end
     end
   end
