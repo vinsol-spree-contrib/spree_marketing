fd31805e3461b0aef69cd9f2578964c0f991fb3b
aishmita-aggarwal <aishmita@vinsol.com>
2016-04-22 14:06:25 +0530

    Add gibbon service to generate and update list on mailchimp

---
+++ app/services/gibbon_service.rb
@@ -0,0 +1,109 @@
+class GibbonService
+
+  attr_reader :members, :list_uids
+
+  MEMBER_STATUS                   = { unsubscribe: 'unsubscribe', subscribe: 'subscribe' }
+  DEFAULT_MEMBER_RETRIEVAL_PARAMS = { params: { 'status': MEMBER_STATUS[:subscribe] } }
+  LIST_ATTRIBUTES                 = [:permission_reminder, :email_type_option, :campaign_defaults, :contact]
+  BATCH_COUNT                     = 50
+  RETRY_COUNT                     = 5
+  TIME_DELAY                      = 500
+
+  def self.gibbon
+    @gibbon ||= Gibbon::Request.new
+  end
+
+  def initialize(list_uids = [])
+    @list_uids = list_uids
+    if @list_uids.any?
+      @members = {} && retrieve_members
+    end
+  end
+
+  def generate_list(list_name = '')
+    response = self.class.gibbon.lists.create(body: { name: list_name }.merge(default_list_generation_params))
+    @list_uid = response['id'] if response['id'].present?
+    response
+  end
+
+  def update_list
+    @list_uid = @list_uids.first
+    unsubscribe_existing_members
+    subscribe_new_members
+    retrieve_members
+  end
+
+  def subscribe_members(members_emails = [])
+    members_batches = members_emails.in_groups_of(BATCH_COUNT, false)
+    members_batches.each do |members_batch|
+      @new_members_emails = members_batch
+      response = self.class.gibbon.batches.create(body: { operations: member_operations_list_to_generate })
+      tail_batch_response(response['batch_id'])
+    end
+  end
+
+  def unsubscribe_members(list_uid = nil)
+    members_batches = @members[list_uid || @list_uid].in_groups_of(BATCH_COUNT, false)
+    members_batches.each do |members_batch|
+      response = self.class.gibbon.batches.create(body: { operations: member_operations_list_to_unsubscribe })
+      tail_batch_response(response['batch_id'])
+    end
+  end
+
+  private
+
+    def tail_batch_response(batch_id)
+      unless check_batch_response(batch_id, RETRY_COUNT)
+        raise Gibbon::MailChimpError.new("Failed Batch #{ batch_id } --- #{ response.body }")
+      end
+    end
+
+    def check_batch_response(batch_id, retry_count)
+      batch_response = self.class.gibbon.batches(batch_id).retrieve
+
+      if batch_response['status'] == 'finished'
+        true
+      else
+        if retry_count > 0
+          sleep(TIME_DELAY)
+          check_batch_response(batch_id, --retry_count)
+        else
+          false
+        end
+      end
+    end
+
+    def retrieve_members
+      @list_uids.select do |list_uid|
+        @members[list_uid] = self.class.gibbon.lists(list_uid).members.retrieve(DEFAULT_MEMBER_RETRIEVAL_PARAMS)
+      end
+    end
+
+    def members_uids
+      members[@list_uid].map { |member| member[:uid] }
+    end
+
+    def member_operations_list_to_unsubscribe
+      member_uids.select do |uid|
+        {
+          method: "PATCH",
+          path: "lists/#{ @list_uid }/members/#{ uid }",
+          body: "{\"status\":\"#{ MEMBER_STATUS[:unsubscribe] }\"}"
+        }
+      end
+    end
+
+    def member_operations_list_to_generate
+      @new_members_emails.select do |email_address|
+        {
+          method: "POST",
+          path: "lists/#{ @list_uid }/members",
+          body: "{\"email_address\": \"#{ email_address }\",\"status\":\"#{ MEMBER_STATUS[:subscribe] }\"}"
+        }
+      end
+    end
+
+    def default_list_generation_params
+      SpreeMarketing::DEFAULT_LIST_GENERATION_PARAMS[Rails.env].slice(*LIST_ATTRIBUTES)
+    end
+end
--- lib/spree_marketing/engine.rb
+++ lib/spree_marketing/engine.rb
@@ -8,6 +8,15 @@ module SpreeMarketing
       g.test_framework :rspec
     end
 
+    initializer 'load spree_marketing config', group: :all do |app|
+      app_config = Rails.root.join('config', 'spree_marketing.yml').to_s
+      if File.exist?( app_config )
+        SpreeMarketing::DEFAULT_LIST_GENERATION_PARAMS = YAML.load_file(app_config)
+      else
+        SpreeMarketing.log "Please create the yml file spree_marketing.yml"
+      end
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
