e233d81a613291289d371a7a0cac5055565fa8e8
VINAY MITTAL <vinay@vinsol.com>
2016-04-25 13:00:18 +0530

    remove null: false from type column in lists

--- db/migrate/20160422080310_add_type_column_to_spree_marketing_lists.rb
+++ db/migrate/20160422080310_add_type_column_to_spree_marketing_lists.rb
@@ -1,5 +1,5 @@
 class AddTypeColumnToSpreeMarketingLists < ActiveRecord::Migration
   def change
-    add_column :spree_marketing_lists, :type, :string, null: false, index: true
+    add_column :spree_marketing_lists, :type, :string, index: true
   end
 end
