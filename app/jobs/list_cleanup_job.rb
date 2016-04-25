3d62e6c641632427713a6ea6fe1192c01f8948e0
aishmita-aggarwal <aishmita@vinsol.com>
2016-04-25 15:15:24 +0530

    Modify list clean up job

--- app/jobs/list_cleanup_job.rb
+++ app/jobs/list_cleanup_job.rb
@@ -4,9 +4,9 @@ class ListCleanupJob < ActiveJob::Base
   # rescue_from(Gibbon::MailChimpError) do |exception|
   # end
 
-  def perform(lists)
+  def perform(list_uids = [])
     gibbon_service = GibbonService.new
-    contacts_data = gibbon_service.delete_lists(lists.pluck(:uid))
-    lists.destroy_all
+    contacts_data = gibbon_service.delete_lists(list_uids)
+    Spree::Marketing::List.where(uid: list_uids).destroy_all
   end
 end
