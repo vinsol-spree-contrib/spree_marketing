d60ce2c915df31158ed5ca99a5c3fb6addee028f
VINAY MITTAL <vinay@vinsol.com>
2016-04-25 15:21:20 +0530

    remove spree_orders.id ordering scope in least active users

--- app/models/spree/marketing/list/least_active_users_list.rb
+++ app/models/spree/marketing/list/least_active_users_list.rb
@@ -7,11 +7,10 @@ module Spree
 
       def user_ids
         Spree::PageEvent.group(:actor_id)
-                        .having("COUNT(spree.page_events.id) < :maximum_count", maximum_count: MAXIMUM_PAGE_EVENT_COUNT)
+                        .having("COUNT(spree_page_events.id) < :maximum_count", maximum_count: MAXIMUM_PAGE_EVENT_COUNT)
                         .of_registered_users
                         .where("created_at >= :time_frame", time_frame: computed_time)
                         .where(actor_type: Spree.user_class)
-                        .order("COUNT(spree_orders.id)")
                         .pluck(:actor_id)
       end
     end
