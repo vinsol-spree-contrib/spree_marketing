0443b89e9011dda5c0bf856ab7c60a320e33f24d
VINAY MITTAL <vinay@vinsol.com>
2016-04-26 12:33:34 +0530

    wrote rsepc for contact model

--- db/migrate/20160422110751_add_timestamp_columns_to_spree_marketing_contacts_lists.rb
+++ db/migrate/20160422110751_add_timestamp_columns_to_spree_marketing_contacts_lists.rb
@@ -1,7 +1,7 @@
 class AddTimestampColumnsToSpreeMarketingContactsLists < ActiveRecord::Migration
   def change
     change_table :spree_marketing_contacts_lists do |t|
-      t.timestamps null: false
+      t.timestamps #null: false
     end
   end
 end
