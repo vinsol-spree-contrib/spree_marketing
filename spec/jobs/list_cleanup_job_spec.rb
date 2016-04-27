68a19befb46d4fd4359d5ecfae7d26969a8cd5f7
aishmita-aggarwal <aishmita@vinsol.com>
2016-04-27 13:49:46 +0530

    Add job rspecs

---
+++ spec/jobs/list_cleanup_job_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+RSpec.describe ListCleanupJob, type: :job do
+  include ActiveJob::TestHelper
+
+  SpreeMarketing::CONFIG ||= { Rails.env => {} }
+
+  class GibbonServiceTest
+  end
+
+  let(:gibbon_service) { GibbonServiceTest.new }
+  let(:list) { create(:marketing_list) }
+
+  before do
+    allow(GibbonService).to receive(:new).and_return(gibbon_service)
+    allow(gibbon_service).to receive(:delete_lists).and_return(true)
+  end
+
+  subject(:job) { described_class.perform_later([list.uid]) }
+
+  it 'queues the job' do
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'destroys lists' do
+    perform_enqueued_jobs { job }
+    expect(Spree::Marketing::List.all).not_to include(list)
+  end
+
+  it 'is in default queue' do
+    expect(ListCleanupJob.new.queue_name).to eq('default')
+  end
+
+  context 'executes perform' do
+    it { expect(GibbonService).to receive(:new).and_return(gibbon_service) }
+    it { expect(gibbon_service).to receive(:delete_lists).with([list.uid]).and_return(true) }
+
+    after { perform_enqueued_jobs { job } }
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+end
---
+++ spec/jobs/list_generation_job_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+RSpec.describe ListGenerationJob, type: :job do
+  include ActiveJob::TestHelper
+
+  SpreeMarketing::CONFIG ||= { Rails.env => {} }
+
+  class GibbonServiceTest; end
+
+  let(:gibbon_service) { GibbonServiceTest.new }
+  let(:list) { create(:marketing_list) }
+  let(:list_name) { 'test' }
+  let(:emails) { ['test@example.com'] }
+  let(:list_class_name) { Spree::Marketing::List.subclasses.first.name }
+  let(:list_data) { { id: '12345678', name: list_name }.with_indifferent_access }
+  let(:contacts_data) { [{ id: '12345678', email_address: emails.first, unique_email_id: 'test' }.with_indifferent_access] }
+
+  before do
+    allow(GibbonService).to receive(:new).and_return(gibbon_service)
+    allow(gibbon_service).to receive(:generate_list).and_return(list_data)
+    allow(gibbon_service).to receive(:subscribe_members).and_return(contacts_data)
+  end
+
+  subject(:job) { described_class.perform_later(list_name, emails, list_class_name) }
+
+  it 'queues the job' do
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'creates lists' do
+    perform_enqueued_jobs { job }
+    expect(Spree::Marketing::List.first.uid).to eq(list_data[:id])
+  end
+
+  it 'adds contacts to generated list' do
+    perform_enqueued_jobs { job }
+    expect(Spree::Marketing::List.first.contacts).not_to be_nil
+  end
+
+  it 'is in default queue' do
+    expect(ListGenerationJob.new.queue_name).to eq('default')
+  end
+
+  context 'executes perform' do
+    it { expect(GibbonService).to receive(:new).and_return(gibbon_service) }
+    it { expect(gibbon_service).to receive(:generate_list).with(list_name).and_return(list_data) }
+    it { expect(gibbon_service).to receive(:subscribe_members).with(emails).and_return(contacts_data) }
+
+    after { perform_enqueued_jobs { job } }
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+end
