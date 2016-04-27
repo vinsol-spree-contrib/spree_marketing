861796d7bf11a3a1b5c5593f842fcf10091c6677
VINAY MITTAL <vinay@vinsol.com>
2016-04-27 17:40:55 +0530

    add abandoned cart list spe

---
+++ spec/models/spree/marketing/list/abandoned_cart_list_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+describe Spree::Marketing::AbandonedCartList, type: :model do
+
+  let!(:first_user) { create(:user) }
+  let!(:second_user) { create(:user) }
+  let!(:registered_user_complete_order) { create(:completed_order_with_totals, user_id: second_user.id) }
+  let!(:registered_user_incomplete_order) { create(:order, user_id: first_user.id) }
+
+  describe "methods" do
+    describe "#user_ids" do
+      context "with incomplete order" do
+        it { expect(Spree::Marketing::AbandonedCartList.new.user_ids).to include first_user.id }
+        it { expect(Spree::Marketing::AbandonedCartList.new.user_ids).to_not include second_user.id }
+      end
+
+      context "user is not registered" do
+        let!(:guest_user_incomplete_order) { create(:order, user_id: nil, email: "spree@example.com") }
+
+        it { expect(Spree::Marketing::AbandonedCartList.new.send :emails).to_not include guest_user_incomplete_order.email  }
+        it { expect(Spree::Marketing::AbandonedCartList.new.send :emails).to include registered_user_incomplete_order.email  }
+      end
+
+      context "updated_at is before TIME FRAME" do
+        let!(:registered_user_old_incomplete_order) { create(:order, user_id: second_user.id, updated_at: Time.current - 10.days) }
+
+        it { expect(Spree::Marketing::AbandonedCartList.new.user_ids).to_not include second_user.id }
+        it { expect(Spree::Marketing::AbandonedCartList.new.user_ids).to include first_user.id }
+      end
+    end
+  end
+
+end
