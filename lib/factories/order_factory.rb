4a29761da350691e7a9ca10a3c53cf201e1017eb
VINAY MITTAL <vinay@vinsol.com>
2016-04-29 14:44:53 +0530

    add factories and context messages and specs for most discounted products

--- lib/factories/order_factory.rb
+++ lib/factories/order_factory.rb
@@ -50,6 +50,23 @@ FactoryGirl.define do
     end
   end
 
+  factory :order_with_promotion, parent: :completed_order_with_totals, class: Spree::Order do
+    after(:create) do |order, evaluator|
+      promotion = create(:promotion_with_order_adjustment)
+      order.promotions << promotion
+    end
+
+    trait :with_custom_completed_at do
+      transient do
+        completed_at nil
+      end
+
+      after(:create) do |order, evaluator|
+        order.update_columns(completed_at: evaluator.completed_at)
+      end
+    end
+  end
+
   factory :guest_user_order_with_given_payment_method, parent: :completed_order_with_totals, class: Spree::Order do
     transient do
       payment_method nil
@@ -59,16 +76,6 @@ FactoryGirl.define do
       payment = create(:payment, amount: order.total, payment_method: evaluator.payment_method)
       order.payments << payment
       order.update(user: nil)
-    end
-  end
-
-  factory :order_with_given_completed_at_and_payment_method, parent: :order_with_given_payment_method, class: Spree::Order do
-    transient do
-      completed_at nil
-    end
-
-    after(:create) do |order, evaluator|
-      order.update_columns(completed_at: evaluator.completed_at)
     end
   end
 
--- spec/models/spree/marketing/list/most_discounted_orders_list_spec.rb
+++ spec/models/spree/marketing/list/most_discounted_orders_list_spec.rb
@@ -2,15 +2,7 @@ require "spec_helper"
 
 describe Spree::Marketing::MostDiscountedOrdersList, type: :model do
 
-  let!(:first_user) { create(:user) }
-  let!(:second_user) { create(:user) }
-  let(:promotion) { create(:promotion_with_order_adjustment) }
-  let!(:first_order) { create(:completed_order_with_totals, promotions: [promotion], user_id: first_user.id) }
-  let!(:second_order) { create(:completed_order_with_totals, promotions: [promotion], user_id: first_user.id) }
-  let!(:third_order) { create(:completed_order_with_totals, promotions: [promotion], user_id: first_user.id) }
-  let!(:fourth_order) { create(:completed_order_with_totals, promotions: [promotion], user_id: first_user.id) }
-  let!(:fifth_order) { create(:completed_order_with_totals, promotions: [promotion], user_id: first_user.id) }
-  let!(:sixth_order) { create(:completed_order_with_totals, promotions: [promotion], user_id: first_user.id) }
+  let!(:user_having_6_completed_orders_with_given_promotion) { create(:user_with_completed_orders, :with_promotion, orders_count: 6) }
 
   describe "Constants" do
     it { expect(Spree::Marketing::MostDiscountedOrdersList::TIME_FRAME).to eq 1.month }
@@ -19,25 +11,27 @@ describe Spree::Marketing::MostDiscountedOrdersList, type: :model do
 
   describe "methods" do
     describe "#user_ids" do
-      context "orders shuld be greater than 5" do
-        let!(:seventh_order) { create(:completed_order_with_totals, promotions: [promotion], user_id: second_user.id) }
+      context "when there are greater than or less than 5 orders" do
+        let(:user_having_4_completed_orders_with_given_promotion) { create(:user_with_completed_orders, :with_promotion, orders_count: 4) }
 
-        it { expect(Spree::Marketing::MostDiscountedOrdersList.new.user_ids).to include first_user.id }
-        it { expect(Spree::Marketing::MostDiscountedOrdersList.new.user_ids).to_not include second_user.id }
+        it { expect(Spree::Marketing::MostDiscountedOrdersList.new.user_ids).to include user_having_6_completed_orders_with_given_promotion.id }
+        it { expect(Spree::Marketing::MostDiscountedOrdersList.new.user_ids).to_not include user_having_4_completed_orders_with_given_promotion.id }
       end
 
-      context "guest user order" do
+      context "when user is not registered" do
         let(:guest_user_email) { "spree@example.com" }
-        let!(:guest_user_order) { create(:completed_order_with_totals, promotions: [promotion], user_id: nil, email: guest_user_email) }
+        let!(:guest_user_order) { create_list(:order_with_promotion, 6, user_id: nil, email: guest_user_email) }
 
         it { expect(Spree::Marketing::MostDiscountedOrdersList.new.send :emails).to_not include guest_user_email }
       end
 
-      context "completed at before TIME_FRAME" do
-        let(:old_completed_order) { create(:completed_order_with_totals, promotions: [promotion], user_id: second_user.id) }
-        before { old_completed_order.update_columns(completed_at: Time.current - 2.month) }
+      context "when orders are completed before TIME_FRAME" do
+        let(:timestamp) { Time.current - 2.months }
+        let(:user_having_6_old_completed_orders_with_given_promotion) { create(:user) }
+        let!(:orders) { create_list(:order_with_promotion, 6, :with_custom_completed_at, completed_at: timestamp, user: user_having_6_old_completed_orders_with_given_promotion) }
 
-        it { expect(Spree::Marketing::MostDiscountedOrdersList.new.user_ids).to_not include second_user.id }
+        it { expect(Spree::Marketing::MostDiscountedOrdersList.new.user_ids).to_not include user_having_6_old_completed_orders_with_given_promotion.id }
+        it { expect(Spree::Marketing::MostDiscountedOrdersList.new.user_ids).to include user_having_6_completed_orders_with_given_promotion.id }
       end
     end
   end
