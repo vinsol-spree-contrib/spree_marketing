674a4b53692ea0da528a778f101e9a6e4661621b
VINAY MITTAL <vinay@vinsol.com>
2016-05-02 18:34:47 +0530

    optimize constant spec for presenters

--- spec/presenters/spree/marketing/list_presenter_spec.rb
+++ spec/presenters/spree/marketing/list_presenter_spec.rb
@@ -8,30 +8,54 @@ describe Spree::Marketing::ListPresenter do
   let(:multilist) { create(:favourable_products_list, product_id: product.id, name: "favourable_products_list_#{ product_name }") }
   let(:list_presenter) { Spree::Marketing::ListPresenter.new list }
   let(:multilist_presenter) { Spree::Marketing::ListPresenter.new multilist }
+  let(:view_names_hash) {
+    {
+      "AbandonedCartList" => {
+        presenter_name: "Abandoned Cart",
+        tooltip_content: "View the contact list of users who have abandoned the cart"
+      },
+      "FavourableProductsList" => {
+        presenter_name: "Most Selling Products",
+        tooltip_content: "View the contact list of users who are part of the purchase family for top 10 most selling products"
+      },
+      "LeastActiveUsersList" => {
+        presenter_name: "Least Active Users",
+        tooltip_content: "View the contact list of users corresponding to least activities"
+      },
+      "LeastZoneWiseOrdersList" => {
+        presenter_name: "Cold Zone",
+        tooltip_content: "View the contact list of users in 5 lowest ordering Zone"
+      },
+      "MostActiveUsersList" => {
+        presenter_name: "Most Active Users",
+        tooltip_content: "View the contact list of users corresponding to most activities"
+      },
+      "MostDiscountedOrdersList" => {
+        presenter_name: "Discount Seekers",
+        tooltip_content: "View the contact list of users who are part of the purchase family mostly for discounted orders"
+      },
+      "MostSearchedKeywordsList" => {
+        presenter_name: "Most Searched Keywords",
+        tooltip_content: "View the contact list of users corresponding to top 10 keywords"
+      },
+      "MostUsedPaymentMethodsList" => {
+        presenter_name: "Most Used Payment Methods",
+        tooltip_content: "View the contact list of users corresponding to most used payment methods"
+      },
+      "MostZoneWiseOrdersList" => {
+        presenter_name: "Hot Zone",
+        tooltip_content: "View the contact list of users in 5 most ordering Zone"
+      },
+      "NewUsersList" => {
+        presenter_name: "New Users",
+        tooltip_content: "View the contact list of users who have signed up in last week"
+      }
+    }
+  }
 
-  describe "Constants" do
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["AbandonedCartList"][:presenter_name]).to eq "Abandoned Cart" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["AbandonedCartList"][:tooltip_content]).to eq "View the contact list of users who have abandoned the cart" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["FavourableProductsList"][:presenter_name]).to eq "Most Selling Products" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["FavourableProductsList"][:tooltip_content]).to eq "View the contact list of users who are part of the purchase family for top 10 most selling products" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["LeastActiveUsersList"][:presenter_name]).to eq "Least Active Users" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["LeastActiveUsersList"][:tooltip_content]).to eq "View the contact list of users corresponding to least activities" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["LeastZoneWiseOrdersList"][:presenter_name]).to eq "Cold Zone" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["LeastZoneWiseOrdersList"][:tooltip_content]).to eq "View the contact list of users in 5 lowest ordering Zone" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostActiveUsersList"][:presenter_name]).to eq "Most Active Users" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostActiveUsersList"][:tooltip_content]).to eq "View the contact list of users corresponding to most activities" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostDiscountedOrdersList"][:presenter_name]).to eq "Discount Seekers" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostDiscountedOrdersList"][:tooltip_content]).to eq "View the contact list of users who are part of the purchase family mostly for discounted orders" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostSearchedKeywordsList"][:presenter_name]).to eq "Most Searched Keywords" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostSearchedKeywordsList"][:tooltip_content]).to eq "View the contact list of users corresponding to top 10 keywords" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostUsedPaymentMethodsList"][:presenter_name]).to eq "Most Used Payment Methods" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostUsedPaymentMethodsList"][:tooltip_content]).to eq "View the contact list of users corresponding to most used payment methods" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostZoneWiseOrdersList"][:presenter_name]).to eq "Hot Zone" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["MostZoneWiseOrdersList"][:tooltip_content]).to eq "View the contact list of users in 5 most ordering Zone" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["NewUsersList"][:presenter_name]).to eq "New Users" }
-    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH["NewUsersList"][:tooltip_content]).to eq "View the contact list of users who have signed up in last week" }
+  describe "Constant" do
+    it { expect(Spree::Marketing::ListPresenter::VIEW_NAMES_HASH).to eq view_names_hash }
   end
-
 
   describe "methods" do
     context "#initialize" do
