adcb580e8add8fe6105c590466a59698fd523c76
VINAY MITTAL <vinay@vinsol.com>
2016-05-03 17:11:09 +0530

    modify context and it block messages as per discussion

--- spec/models/spree/marketing/list/most_active_users_list_spec.rb
+++ spec/models/spree/marketing/list/most_active_users_list_spec.rb
@@ -12,30 +12,30 @@ describe Spree::Marketing::MostActiveUsersList, type: :model do
 
   describe "methods" do
     describe "#user_ids" do
-      context "user having less than 5 page events" do
+      context "when there are users having less than 5 page events" do
         let!(:user_with_4_page_events) { create(:user_with_page_events, page_events_count: 4) }
 
-        it "include users having more than 5 page events" do
+        it "return user ids which include users having more than 5 page events" do
           expect(Spree::Marketing::MostActiveUsersList.new.user_ids).to include user_with_6_page_events.id
         end
-        it "doesn't include users having less than 5 page events" do
+        it "return user ids which doesn't include users having less than 5 page events" do
           expect(Spree::Marketing::MostActiveUsersList.new.user_ids).to_not include user_with_4_page_events.id
         end
       end
 
-      context "with guest users page events" do
+      context "when there are page events of guest users" do
         let!(:guest_user_page_event) { create_list(:marketing_page_event, 6, actor: nil) }
 
-        it "doesn't include guest users" do
+        it "return user ids which doesn't include guest users" do
           expect(Spree::Marketing::MostActiveUsersList.new.user_ids).to_not include nil
         end
       end
 
-      context "with page events of users other than Spree.user_class type" do
+      context "when there are page events of users other than Spree.user_class type" do
         let(:other_user_type_id) { 9 }
         let!(:other_user_type_page_events) { create_list(:marketing_page_event, 6, actor_id: other_user_type_id, actor_type: nil) }
 
-        it "doesn't include users other than Spree.user_class type" do
+        it "return user ids which doesn't include users other than Spree.user_class type" do
           expect(Spree::Marketing::MostActiveUsersList.new.user_ids).to_not include other_user_type_id
         end
       end
@@ -44,10 +44,10 @@ describe Spree::Marketing::MostActiveUsersList, type: :model do
         let(:timestamp) { Time.current - 10.days }
         let!(:user_with_6_old_page_events) { create(:user_with_page_events, page_events_count: 6, created_at: timestamp) }
 
-        it "include users having page events created after time frame" do
+        it "return user ids which include users having page events created after time frame" do
           expect(Spree::Marketing::MostActiveUsersList.new.user_ids).to include user_with_6_page_events.id
         end
-        it "doesn't include users having page events created before time frame" do
+        it "return user ids which doesn't include users having page events created before time frame" do
           expect(Spree::Marketing::MostActiveUsersList.new.user_ids).to_not include user_with_6_old_page_events.id
         end
       end
