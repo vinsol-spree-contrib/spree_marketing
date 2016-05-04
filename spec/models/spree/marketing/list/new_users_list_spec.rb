a6873f49647390c8a7577e59cf4a07d357ad3879
aishmita-aggarwal <aishmita@vinsol.com>
2016-05-04 12:14:54 +0530

    Modify rspec for new_users_list

--- spec/models/spree/marketing/list/new_users_list_spec.rb
+++ spec/models/spree/marketing/list/new_users_list_spec.rb
@@ -5,6 +5,12 @@ describe Spree::Marketing::NewUsersList, type: :model do
   let!(:new_user) { create(:user) }
   let!(:old_user) { create(:user, created_at: Time.current - 10.days) }
 
+  describe 'Constants' do
+    it 'NAME_TEXT equals to name representation for list' do
+      expect(Spree::Marketing::NewUsersList::NAME_TEXT).to eq 'New Users'
+    end
+  end
+
   describe "methods" do
     context "#emails" do
       it { expect(Spree::Marketing::NewUsersList.new.send :emails).to include new_user.email }
