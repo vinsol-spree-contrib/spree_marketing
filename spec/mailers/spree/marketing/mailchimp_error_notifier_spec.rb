b1f6b65e2fa2e0990dcc8f78065f856125d8de07
aishmita-aggarwal <aishmita@vinsol.com>
2016-05-06 17:40:38 +0530

    Add rspec for error notifier

---
+++ spec/mailers/spree/marketing/mailchimp_error_notifier_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe Spree::Marketing::MailchimpErrorNotifier do
+
+  SpreeMarketing::CONFIG = { Rails.env => { campaign_defaults: { from_email: 'a@test.com' }} }
+
+  describe 'notify_failure' do
+
+    let(:mail) { described_class.notify_failure('TestJob', 'title', 'detail') }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq I18n.t(:subject, scope: [:spree, :marketing, :mailchimp_error_notifier, :notify_failure])
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq [SpreeMarketing::CONFIG[Rails.env][:campaign_defaults][:from_email]]
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq ['marketing@vinsol.com']
+    end
+  end
+end
