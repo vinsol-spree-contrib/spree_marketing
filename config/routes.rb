3176dff40c53a08a7366da47bcbc3275e88d5a16
aishmita-aggarwal <aishmita@vinsol.com>
2016-05-12 16:05:13 +0530

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Spree::Core::Engine.routes.draw do
   namespace :admin do
     namespace :marketing do
       resources :lists, only: [:show, :index]
-      resources :campaigns, only: [:show, :index]
+      resources :campaigns, only: [:show, :index] do
+        member do
+          get :display_recipient_emails
+        end
+      end
       post 'campaigns/sync', to: 'campaigns#sync'
-      get 'campaigns/display_recipient_emails', to: 'campaigns#display_recipient_emails'
     end
   end
 
