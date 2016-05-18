4c02aa110a8a9d32d00a680c564c7bb97fa4c8a4
VINAY MITTAL <vinay@vinsol.com>
2016-05-18 17:09:22 +0530

    fix ruby hash issues and add views

--- app/controllers/spree/admin/marketing/campaigns_controller.rb
+++ app/controllers/spree/admin/marketing/campaigns_controller.rb
@@ -15,7 +15,7 @@ module Spree
         def display_recipient_emails
           @report_name = params[:report_key]
           @recipients = @marketing_campaign.recipients.with_emails(@reports[params[:report_key]]['emails']).page(params[:page]).per(20)
-          @first_activity_of_users_hash = @recipients.public_send((params[:report_key] + "_data").to_sym, @campaign)
+          @users_activity_hash = @recipients.public_send((params[:report_key] + "_data").to_sym, @campaign)
         end
 
         private
