03cc28f9e3a4cd7811d743d71b8bd6f5d9b1d051
SergeyMell <sergey.mell@agilie.com>
2019-01-24 18:52:06 +0200

    Fixed name text fetching

--- app/services/gibbon_service/campaign_service.rb
+++ app/services/gibbon_service/campaign_service.rb
@@ -7,21 +7,21 @@ module GibbonService
     def retrieve_sent_campaigns(since_send_time = nil)
       params = { params: { since_send_time: since_send_time || Spree::Marketing::Campaign::DEFAULT_SEND_TIME_GAP.ago.to_s } }
       p "Retrieving campaigns sent since #{since_send_time}"
-      campaigns = gibbon.campaigns.retrieve(params)['campaigns']
+      campaigns = gibbon.campaigns.retrieve(params).body['campaigns']
       p 'Retrieved campaigns'
       campaigns
     end
 
     def retrieve_report(campaign_id = nil)
       p "Retrieving report for campaign_id #{campaign_id}"
-      report = gibbon.reports(campaign_id || @campaign_id).retrieve
+      report = gibbon.reports(campaign_id || @campaign_id).retrieve.body
       p 'Retrieved report'
       report
     end
 
     def retrieve_recipients(campaign_id = nil)
       p "Retrieving recipients for campaign_id #{campaign_id}"
-      @recipients = gibbon.reports(campaign_id || @campaign_id).sent_to.retrieve['sent_to']
+      @recipients = gibbon.reports(campaign_id || @campaign_id).sent_to.retrieve.body['sent_to']
       p 'Retrieved recipients'
       @recipients
     end
